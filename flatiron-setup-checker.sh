05851272b6f8b055b80e49c70b85247469a3ba1c
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-03-09 17:42:06 +0000

    extracted strings, wrapped all installs in check functions

--- flatiron-setup-checker.sh
+++ flatiron-setup-checker.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 ###########################################################################
-# Credit to Hysan for this epic checker. Copied and changed from original #
+# Credit to Hysan for this script. Copied and changed from original #
 ###########################################################################
 
-
-# Todo. Change Ruby version
 
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 NC='\033[0m' # No Color
@@ -15,7 +13,6 @@ BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 
 
-# https://stackoverflow.com/questions/6212219/passing-parameters-to-a-bash-function
 # https://stackoverflow.com/questions/5615717/how-to-store-a-command-in-a-variable-in-linux
 # https://stackoverflow.com/questions/17336915/return-value-in-a-bash-function
 # $1 => Command to run
@@ -40,7 +37,6 @@ print_table_results () {
 # $2 => Command to run
 print_data_row () {
   local result=$(evaluate "$2")
-  # https://stackoverflow.com/questions/6345429/how-do-i-print-some-text-in-bash-and-pad-it-with-spaces-to-a-certain-width
   printf "%-12s => [ %-6s ]\n" "$1" "$result"
 }
 
@@ -61,8 +57,6 @@ configuration_header () {
 validation_header
 delimiter
 
-## 1. Uninstall Learn IDE
-## TODO
 
 ## 2. Install Xcode Command Line Tools
 # https://apple.stackexchange.com/questions/219507/best-way-to-check-in-bash-if-command-line-tools-are-installed
@@ -71,84 +65,74 @@ delimiter
 print_table_results "Xcode Command Line Tools" 'type xcode-select >&- && xpath=$( xcode-select --print-path ) && test -d "${xpath}" && test -x "${xpath}"'
 delimiter
 
-## 3. N/A
-
 ## 4. Homebrew
 # https://stackoverflow.com/questions/21577968/how-to-tell-if-homebrew-is-installed-on-mac-os-x
 # https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
-print_table_results "Homebrew" "command -v brew >/dev/null 2>&1"
+print_table_results "Homebrew" $check_brew
 delimiter
 
 ## 5. git
 # https://stackoverflow.com/questions/12254076/how-do-i-show-my-global-git-config
-print_table_results "Installed git" "command -v git >/dev/null 2>&1 && git version | grep -q 'git version'"
-print_table_results "Github user config" "command -v git >/dev/null 2>&1 && git config --list | grep -q 'github.user='"
-print_table_results "Github email config" "command -v git >/dev/null 2>&1 && git config --list | grep -q 'github.email='"
+print_table_results "Installed git" $check_git
+print_table_results "Github user config" $check_git_user
+print_table_results "Github email config" $check_git_email
 delimiter
 
 ## 6. Support Libraries
-print_table_results "Installed gmp" "command -v brew >/dev/null 2>&1 && brew list | grep -q 'gmp'"
-print_table_results "Installed gnupg" "command -v brew >/dev/null 2>&1 && brew list | grep -q 'gnupg'"
+print_table_results "Installed gmp" $check_gmp
+print_table_results "Installed gnupg" $check_gnupg
 delimiter
 
 ## 7. Ruby Version Manager (rvm)
-print_table_results "Installed RVM" "command -v rvm >/dev/null 2>&1 && which rvm | grep -q '/Users/.*/\.rvm/bin/rvm'"
-print_table_results "Default RVM (2.3.3)" "command -v rvm >/dev/null 2>&1 && rvm list | grep -Fq '=* ruby-2.6.0 [ x86_64 ]'"
-print_table_results "Test RVM PATH" "command -v rvm >/dev/null 2>&1 && rvm list | grep -Fqv 'Warning! PATH'"
+print_table_results "Installed RVM" $check_rvm
+print_table_results "Default RVM (2.3.3)" $check_rvm_v
+print_table_results "Test RVM PATH" $check_rvm_path
 delimiter
 
 ## 8. Gems
-print_table_results "Gem: learn-co" "command -v gem >/dev/null 2>&1 && gem list | grep -q 'learn-co'"
-print_table_results "Gem: bundler" "command -v gem >/dev/null 2>&1 && gem list | grep -q 'bundler'"
+print_table_results "Gem: learn-co" $check_learn
+print_table_results "Gem: bundler" $check_bundler
 delimiter
 
 ## 9. Learn
 ## See Student Configuration section.
 
 ## 10. Atom
-print_table_results "Installed Atom" "command -v atom >/dev/null 2>&1 && atom -v | grep -q 'Atom'"
-print_table_results "Learn Editor" "cat ~/.learn-config | grep ':editor:' | grep -q 'atom'"
+print_table_results "Installed Atom" $check_atom
+print_table_results "Learn Editor" $check_atom_editor
 delimiter
 
 ## 11. Gems (more)
-print_table_results "Gem: phantomjs" "command -v gem >/dev/null 2>&1 && gem list | grep -q 'phantomjs'"
-print_table_results "Gem: nokogiri" "command -v gem >/dev/null 2>&1 && gem list | grep -q 'nokogiri'"
+print_table_results "Gem: nokogiri" $check_nokogiri
 delimiter
 
 ## 12. Databases
-print_table_results "Installed sqlite" "command -v sqlite3 >/dev/null 2>&1"
-print_table_results "Installed PostgreSQL" "command -v postgres >/dev/null 2>&1 && postgres --version | grep -q 'postgres (PostgreSQL)'"
-print_table_results "Installed psql" "command -v psql >/dev/null 2>&1 && psql --version | grep -q 'psql (PostgreSQL)'"
+print_table_results "Installed sqlite" $check_sqlite
+print_table_results "Installed PostgreSQL" $check_postgresql
+print_table_results "Installed psql" $check_psql
 delimiter
 
 ## 13. Rails
-print_table_results "Installed Rails" "command -v rails >/dev/null 2>&1 && rails --version | grep -q 'Rails'"
-print_table_results "Gem: rails" "command -v gem >/dev/null 2>&1 && gem list | grep -q 'rails'"
+print_table_results "Installed Rails" $check_rail
+print_table_results "Gem: rails" $check_rails_gem
 delimiter
 
 ## 14. Node Version Manager (nvm)
 # https://unix.stackexchange.com/questions/184508/nvm-command-not-available-in-bash-script
 # https://stackoverflow.com/questions/39190575/bash-script-for-changing-nvm-node-version
 . ~/.nvm/nvm.sh
-print_table_results "Installed NVM" "command -v nvm >/dev/null 2>&1 && nvm --version | grep -q '[0-9]*\.[0-9]*\.[0-9]*'"
-print_table_results "Installed Node" "command -v node | grep -q '/Users/.*/.nvm/versions/node/v.*/bin/node'"
+print_table_results "Installed NVM" $check_nvm
+print_table_results "Installed Node" $check_node
 print_table_results "Default Node (11.x)" 'command -v nvm >/dev/null 2>&1 && nvm version default | grep -q "v11"'
-# print_table_results "Default Node (10.x)" 'command -v nvm >/dev/null 2>&1 && nvm version default | grep -q "v10"'
-# print_table_results "Default Node (6.11.2)" 'command -v nvm >/dev/null 2>&1 && nvm version default | grep -q "v6.11.2"'
 delimiter
-
-## 15. Java IDC
-# https://stackoverflow.com/questions/36388348/check-if-java-installed-with-bash
-# print_table_results "Installed Java" 'command -v java >/dev/null 2>&1 && java -version 2>&1 >/dev/null | grep -q "java version"'
-# delimiter
 
 ## 16. Google Chrome
 # https://unix.stackexchange.com/questions/63387/single-command-to-check-if-file-exists-and-print-custom-message-to-stdout
-print_table_results "Installed Google Chrome" "[ -f /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ] && /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version | grep -q 'Google Chrome'"
+print_table_results "Installed Google Chrome" $check_chrome
 delimiter
 
 ## 17. Slack
-print_table_results "Installed Slack" "[ -f /Applications/Slack.app/Contents/MacOS/Slack ] && /Applications/Slack.app/Contents/MacOS/Slack --version | grep -q ''"
+print_table_results "Installed Slack" $check_slack
 delimiter
 
 
@@ -158,14 +142,14 @@ delimiter
 
 ## 5. git
 echo "Github"
-print_data_row "Username" "command -v git >/dev/null 2>&1 && git config github.user"
-print_data_row "Email" "command -v git >/dev/null 2>&1 && git config github.email"
+print_data_row "Username" $check_git_config_username
+print_data_row "Email" $check_git_config_email
 delimiter
 
 ## 9. Learn
 # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
 echo "Learn"
-print_data_row "Name" "command -v learn >/dev/null 2>&1 && learn whoami | grep 'Name:' | sed 's/Name://g' | sed -e 's/^[[:space:]]*//'"
-print_data_row "Username" "command -v learn >/dev/null 2>&1 && learn whoami | grep 'Username:' | sed 's/Username://g' | sed -e 's/^[[:space:]]*//'"
-print_data_row "Email" "command -v learn >/dev/null 2>&1 && learn whoami | grep 'Email:' | sed 's/Email://g' | sed -e 's/^[[:space:]]*//'"
+print_data_row "Name" $check_learn_name
+print_data_row "Username" $check_learn_username
+print_data_row "Email" $check_learn_email
 delimiter
--- flatiron-setup.sh
+++ flatiron-setup.sh
@@ -2,34 +2,35 @@
 
 # get all the input at the start for those UX points
 echo "Please ensure you are connected to wifi!"
+source ./checker_strings.sh
 
 read -p "Enter github email: " email
 read -p "Enter fullname: " fullname
 
-# xcode is by far the hardest part to install automatically. Errors in the script will most likley arise from this.
-# xcode-select --install && sleep 2
-# osascript -e 'tell application "System Events"' -e 'tell process "Install Command Line Developer Tools"' -e 'keystroke return' -e 'click button "Agree" of window "License Agreement"' -e 'end tell' -e 'end tell'
-
-os=$(sw_vers -productVersion | awk -F. '{print $1 "." $2}')
-if softwareupdate --history | grep --silent "Command Line Tools.*${os}"; then
-    echo 'Command-line tools already installed.' 
-else
-    echo 'Installing Command-line tools...'
-    in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-    touch ${in_progress}
-    product=$(softwareupdate --list | awk "/\* Command Line.*${os}/ { sub(/^   \* /, \"\"); print }")
-    softwareupdate --verbose --install "${product}" || echo 'Installation failed.' 1>&2 && rm ${in_progress} && exit 1
-    rm ${in_progress}
-    echo 'Installation succeeded.'
-fi
-
-echo "you now have xcode :)"
+####################################################
+# xcode is by far the hardest part to install automatically. So I've skipped it.
+#
+# os=$(sw_vers -productVersion | awk -F. '{print $1 "." $2}')
+# if softwareupdate --history | grep --silent "Command Line Tools.*${os}"; then
+#     echo 'Command-line tools already installed.'
+# else
+#     echo 'Installing Command-line tools...'
+#     in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+#     touch ${in_progress}
+#     product=$(softwareupdate --list | awk "/\* Command Line.*${os}/ { sub(/^   \* /, \"\"); print }")
+#     softwareupdate --verbose --install "${product}" || echo 'Installation failed.' 1>&2 && rm ${in_progress} && exit 1
+#     rm ${in_progress}
+#     echo 'Installation succeeded.'
+# fi
+######################################################
 
 # homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
+if ! eval $check_brew; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+  echo "you now have homebrew"
+fi
 
-echo "you now have homebrew"
 
 # general brew packages
 function install_or_upgrade { brew ls | grep $1 > /dev/null; if (($? == 0)); then brew upgrade $1; else brew install $1; fi }
@@ -42,84 +43,83 @@ install_or_upgrade "gmp"
 install_or_upgrade "gnupg"
 install_or_upgrade "sqlite"
 
-echo "you now have homebrew level 2"
-
-# needs a checker for  Warning: gnupg-1.4.19 --->  brew link gnupg
+echo "you now have homebrew lvl 2"
 
 # lets have some rvm fun
-curl -sSL https://get.rvm.io | bash
-
-# work around so you dont have to close and reopen terminal
-if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
-  . "$HOME/.rvm/scripts/rvm"
-else
-  echo "$HOME/.rvm/scripts/rvm" could not be found.
-  exit 1
+eval $check_rvm
+if (($? == 1)); then
+  curl -sSL https://get.rvm.io | bash
+  # work around so you dont have to close and reopen terminal
+  if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
+    . "$HOME/.rvm/scripts/rvm"
+  else
+    echo "$HOME/.rvm/scripts/rvm" could not be found.
+    exit 1
+  fi
+  export PATH="$PATH:$HOME/.rvm/bin"
+  echo "you now have rvm (that's ruby version manager)"
 fi
-
-export PATH="$PATH:$HOME/.rvm/bin"
-
-echo "you now have rvm (that's ruby version manager)"
 
 ####################################################################
 # Ruby version
 #
 # Current flatiron version
-# vers=2.5.3
-
+vers=2.3.3
+#
 # Or keep up to date!!
-# A technique to find the latest stable version of ruby.  Currently this script default to 2.6.1 
+# A technique to find the latest stable version of ruby.
 # html = Net::HTTP.get(URI("https://www.ruby-lang.org/en/downloads/"))
 # vers = html[/http.*ruby-(.*).tar.gz/,1]
 ####################################################################
-vers=2.6.1
 
 
-# necessary for rvm to become a shell function, and so to run rvm use...
-source  ~/.rvm/scripts/rvm
-rvm use $vers --default --install
-
-echo "you now have ruby $vers. Yay!"
+if ! eval $check_ruby_version; then
+  # necessary for rvm to become a shell function, and so to run rvm use...
+  source  ~/.rvm/scripts/rvm
+  rvm use $vers --default --install
+  echo "you now have ruby $vers. Yay!"
+fi
 
 # we love gems.
 gem update --system
 gem install learn-co bundler json rspec pry pry-byebug nokogiri hub thin shotgun rack hotloader rails sinatra --no-document
-
 echo "you now have ruby with gems!!"
 
-# leaving this because it requires input 
-# learn whoami
-
 # need to do github stuff
-git config --global user.email $email 
+git config --global user.email $email
 git config --global user.name $fullname
 
 # Cask for slack, google and atom
-brew install caskroom/cask/brew-cask
-brew cask install atom
+eval $check_cask && brew install caskroom/cask/brew-cask
+eval $check_atom && brew cask install atom
 
-echo "Atomified." 
+eval $check_nvm
+if (( $? == 1)); then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+  echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
+  echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.bash_profile
+  source ~/.bash_profile
+fi
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
-echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.bash_profile
-source ~/.bash_profile
-
-brew cask install google-chrome
-brew cask install slack
+eval $check_chrome && brew cask install google-chrome
+eval $check_slack && brew cask install slack
 
 echo "Cask that task"
 
 # node stuff
-nvm install node
-nvm use node
-nvm alias default node
+if ! eval $check_node; then
+  nvm install node
+  nvm use node
+  nvm alias default node
+fi
 
-# pg
-brew install postgresql
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-export alias pg_start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
-pg_start
+# postgres
+if ! eval $check_postgres
+  brew install postgresql
+  ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+  export alias pg_start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+  pg_start
+fi
 
 # This script is far from perfect. Check it.
 curl -so- https://raw.githubusercontent.com/Voxoff/flatiron-manual-setup/master/flatiron-setup-checker.sh | bash 2> /dev/null
