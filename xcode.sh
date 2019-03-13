8ba4bda49cdda7178ad4ea5fad81a03019dbd7ce
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-03-13 10:07:28 +0000

    one big script for delivery

--- xcode.sh
+++
@@ -1,9 +0,0 @@
-check=$((xcode-\select --install) 2>&1)
-echo $check
-str="xcode-select: note: install requested for command line developer tools"
-while [[ "$check" == "$str" ]];
-do
-  osascript -e 'tell app "System Events" to display dialog "xcode command-line tools missing." buttons "OK" default button 1 with title "xcode command-line tools"'
-  exit;  
-done
-
