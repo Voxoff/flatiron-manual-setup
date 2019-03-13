e65bd695dfb108337eb859d5fed86b15e7555ced
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-03-13 11:55:55 +0000

    echo new line

--- flatiron-setup.sh
+++ flatiron-setup.sh
@@ -56,7 +56,7 @@ echo "If errors arise, ensure you have run 'xcode-select --install'"
 printf "Enter github email: "
 read ans
 email=$ans
-
+echo "\n"
 printf 'Enter github name: '
 read ans
 name=$ans
